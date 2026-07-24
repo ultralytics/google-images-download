dd31a416e2df27f61991b26bbf9b26599f2ee1fd
Glenn Jocher <glenn.jocher@ultralytics.com>
2026-07-24 02:01:22 +0200

    Preserve timestamp key format

--- bing_scraper.py
+++ bing_scraper.py
@@ -1179,12 +1179,12 @@ class googleimagesdownload:
         # Setting limit on number of images to be downloaded
         limit = int(arguments["limit"]) if arguments["limit"] else 100
         if arguments["url"]:
-            current_time = str(datetime.datetime.now().astimezone()).split(".")[0]
-            search_keyword = [current_time.replace(":", "_")]
+            current_time = datetime.datetime.now().astimezone().strftime("%Y-%m-%d %H_%M_%S")
+            search_keyword = [current_time]
 
         if arguments["similar_images"]:
-            current_time = str(datetime.datetime.now().astimezone()).split(".")[0]
-            search_keyword = [current_time.replace(":", "_")]
+            current_time = datetime.datetime.now().astimezone().strftime("%Y-%m-%d %H_%M_%S")
+            search_keyword = [current_time]
 
         # If single_image or url argument not present then keywords is mandatory argument
         if (
